650f3e2e7b235ce95f666a1bae2669e4a06f0f41
Jan Miksovsky <jan@quickui.org>
2015-10-15 10:50:24 -0700

    Rough in marshalling of attributes to properties.

--- ElementBase.js
+++ ElementBase.js
@@ -2,25 +2,44 @@
 
 class ElementBase extends HTMLElement {
 
+  // Handle a change to the attribute with the given name.
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.log(`attribute ${name} changed to ${newValue}`);
+    // If the attribute name corresponds to a property name, then set that
+    // property.
+    // TODO: This looks up the existence of the property each time. It would
+    // be more efficient to, e.g., do a one-time computation of all properties
+    // defined by the element (including base classes).
+    // TODO: Ignore standard attribute name.
+    // TODO: Map hyphenated foo-bar attribute names to camel case fooBar names.
+    if (hasProperty(this, name)) {
+      this[name] = newValue;
+    }
+  }
+
   createdCallback() {
     this.log("created");
-    let template = this.template;
-    if (template) {
-      if (typeof template === 'string') {
-        // Upgrade plain string to real template.
-        template = createTemplateWithInnerHTML(template);
-      }
-      this.log("cloning template into shadow root");
-      this.root = this.createShadowRoot();
-      let clone = document.importNode(template.content, true);
-      this.root.appendChild(clone);
+    if (this.template) {
+      createShadowRootWithTemplate(this, this.template);
     }
+    marshallAttributesToProperties(this);
   }
 
   log(text) {
     console.log(`${this.localName}: ${text}`);
   }
 
+}
+
+function createShadowRootWithTemplate(element, template) {
+  if (typeof template === 'string') {
+    // Upgrade plain string to real template.
+    template = createTemplateWithInnerHTML(template);
+  }
+  element.log("cloning template into shadow root");
+  element.root = element.createShadowRoot();
+  let clone = document.importNode(template.content, true);
+  element.root.appendChild(clone);
 }
 
 function createTemplateWithInnerHTML(innerHTML) {
@@ -34,6 +53,22 @@ function createTemplateWithInnerHTML(innerHTML) {
     template.content.appendChild(div.childNodes[0]);
   }
   return template;
+}
+
+function hasProperty(obj, name) {
+  if (!obj) {
+    return false;
+  } else if (obj.hasOwnProperty(name)) {
+    return true;
+  } else {
+    return hasProperty(Object.getPrototypeOf(obj), name);
+  }
+}
+
+function marshallAttributesToProperties(element) {
+  [].forEach.call(element.attributes, (attribute) => {
+    element.attributeChangedCallback(attribute.name, undefined, attribute.value);
+  });
 }
 
 document.registerElement('element-base', ElementBase);
