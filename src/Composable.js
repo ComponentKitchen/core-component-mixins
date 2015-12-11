ba0d2aab0be8e4b2fae4c86268bbd1a07a809a2b
Jan Miksovsky <jan@quickui.org>
2015-12-10 17:27:03 -0800

    Get ES5 example working again.

--- src/Composable.js
+++ src/Composable.js
@@ -11,6 +11,16 @@ export default (base) => class Composable extends base {
 };
 
 
+// Properties defined by Object that we don't want to mixin.
+const NON_MIXABLE_OBJECT_PROPERTIES = [
+  'constructor'
+];
+
+/*
+ * Apply the mixin to the given base class to return a new class.
+ * The mixin can either be a function that returns the modified class, or a
+ * plain object whose members will be copied to the new class' prototype.
+ */
 function composeClass(base, mixin) {
   if (typeof mixin === 'function') {
     // Mixin function
@@ -18,7 +28,22 @@ function composeClass(base, mixin) {
   } else {
     // Mixin object
     class Subclass extends base {}
-    Object.assign(Subclass.prototype, mixin);
+    copyOwnProperties(mixin, Subclass.prototype, NON_MIXABLE_OBJECT_PROPERTIES);
     return Subclass;
   }
+}
+
+
+/*
+ * Copy the given properties/methods to the target.
+ * Return the updated target.
+ */
+function copyOwnProperties(source, target, ignorePropertyNames = []) {
+  Object.getOwnPropertyNames(source).forEach(name => {
+    if (ignorePropertyNames.indexOf(name) < 0) {
+      let descriptor = Object.getOwnPropertyDescriptor(source, name);
+      Object.defineProperty(target, name, descriptor);
+    }
+  });
+  return target;
 }
