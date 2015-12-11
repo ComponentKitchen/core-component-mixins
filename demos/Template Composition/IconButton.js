3de9530b3ffb4fcf286b45cbd27c2ef08c377ef0
Jan Miksovsky <jan@quickui.org>
2015-12-10 16:34:44 -0800

    Rework TemplateComposition from a mixin to a helper function.

--- demos/Template Composition/IconButton.js
+++ demos/Template Composition/IconButton.js
@@ -1,9 +1,9 @@
-/* A demonstration of the TemplateComposition mixin. */
+/* A demonstration of template composition. */
 
 
 import Composable from 'Composable/src/Composable';
 import SimpleButton from './SimpleButton';
-import { foldIntoBaseSlots } from '../../src/TemplateComposition';
+import composeTemplates from '../../src/composeTemplates';
 
 
 export default class IconButton extends SimpleButton {
@@ -16,9 +16,8 @@ export default class IconButton extends SimpleButton {
     this.$.icon.src = value;
   }
 
-  // @Composable.rule(foldIntoBaseSlots)
   get template() {
-    return foldTemplate(super.template, `
+    return composeTemplates(super.template, `
        <style>
        #icon {
          height: 0.8em;
@@ -32,69 +31,6 @@ export default class IconButton extends SimpleButton {
   }
 
 }
-// Indicate that IconButton should have its template folded into the template
-// defined by the SimpleButton base class. This effectively inserts IconButtons'
-// template into the <content> slot defined by SimpleButton.
-// Composable.decorate.call(IconButton.prototype, {
-//   template: Composable.rule(foldIntoBaseSlots)
-// });
 
-
-// export default IconButton = SimpleButton.compose(IconButton);
 
 document.registerElement('icon-button', IconButton);
-
-
-
-function foldTemplate(baseTemplate, mixinTemplate) {
-
-  mixinTemplate = makeTemplate(mixinTemplate);
-  if (!baseTemplate) {
-    // No folding necessary.
-    return mixinTemplate;
-  }
-
-  baseTemplate = makeTemplate(baseTemplate);
-
-  let mixinElement = mixinTemplate && mixinTemplate.content.cloneNode(true);
-  let baseElement = baseTemplate && baseTemplate.content.cloneNode(true);
-
-  let folded = document.createElement('template');
-
-  // Fold mixin template into first slot element in base template.
-  // TODO: Support named slots.
-  let slotNode = baseElement.querySelector('slot');
-  if (slotNode) {
-    slotNode.parentNode.replaceChild(mixinElement, slotNode);
-    folded.content.appendChild(baseElement);
-  } else {
-    // No place in base for mixin template -- throw mixin template away.
-    folded.content.appendChild(baseElement);
-  }
-
-  return folded;
-
-}
-
-
-function makeTemplate(htmlOrTemplate) {
-  return typeof htmlOrTemplate === 'string' ?
-    createTemplateWithInnerHTML(htmlOrTemplate) :
-    htmlOrTemplate;
-}
-
-
-// TODO: Share with TemplateStamping.
-// Convert a plain string of HTML into a real template element.
-function createTemplateWithInnerHTML(innerHTML) {
-  let template = document.createElement('template');
-  // REVIEW: Is there an easier way to do this?
-  // We'd like to just set innerHTML on the template content, but since it's
-  // a DocumentFragment, that doesn't work.
-  let div = document.createElement('div');
-  div.innerHTML = innerHTML;
-  while (div.childNodes.length > 0) {
-    template.content.appendChild(div.childNodes[0]);
-  }
-  return template;
-}
