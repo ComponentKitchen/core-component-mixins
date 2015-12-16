f96f61f143c2599d7ecb9ee871177c10a16c5f47
Jan Miksovsky <jan@quickui.org>
2015-12-16 11:21:59 -0800

    Add comments to Composable mixin.

--- src/Composable.js
+++ src/Composable.js
@@ -1,5 +1,37 @@
+/*
+ * Mixin to make a class more easily composable with other mixins.
+ *
+ * The main contribution is the introduction of a `compose` method that applies
+ * a set of mixin functions and returns the resulting new class. This sugar
+ * can make the application of many mixins at once easier to read.
+ */
+
+
 export default (base) => class Composable extends base {
 
+  /**
+   * Apply a set of mixin functions or mixin objects to the present class and
+   * return the new class.
+   *
+   * A call like
+   *
+   *     let MyClass = Mixin1(Mixin2(Mixin3(Mixin4(Mixin5(BaseClass)))));
+   *
+   * Can be converted to:
+   *
+   *     let MyClass = Composable(BaseClass).compose(
+   *       Mixin1,
+   *       Mixin2,
+   *       Mixin3,
+   *       Mixin4,
+   *       Mixin5
+   *     );
+   *
+   * This function can also take mixin objects. A mixin object is just a
+   * shorthand for a mixin function that creates a new subclass with the given
+   * members. The mixin object's members are *not* copied directly onto the
+   * prototype of the base class, as with traditional mixins.
+   */
   static compose(...mixins) {
     // We create a new subclass for each mixin in turn. The result becomes
     // the base class extended by any subsequent mixins. It turns out that
